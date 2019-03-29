3d02f23cad9e607a5d18b60c409b6cdd11ca95b9
Steven <dev.stevendaye@yahoo.com>
2019-03-29 12:46:36 +0100

    Created the Post Model and storing it in memory for a while

---
+++ models/Posts.js
@@ -0,0 +1,21 @@
+/* ## Creating the Post Object ## */
+// Hiding data implementation with Symbol
+const _post_key = Symbol("key");
+const _post_title = Symbol("title");
+const _post_body = Symbol("body");
+
+class Post {
+  constructor (key, title, body) {
+    this[_post_key] = key;
+    this[_post_title] = title;
+    this[_post_body] = body;
+  }
+
+  get key() { return this[_post_key]; }
+  get title() { return this[_post_title] }
+  set title() { return this[_post_title] = newTitle }
+  get body() { return this[_post_body] }
+  set body() { return this[_post_body] = newBody }
+}
+
+module.exports = Post;
---
+++ models/posts-memory.js
@@ -0,0 +1,36 @@
+/* ## Storting Posts in Memeory ## */
+const Post = require("./Posts");
+
+var posts = [];
+
+// Creating a Post
+exports.create = async (key, title, body) => {
+  posts[key] = new Post(key, title, body);
+  return posts[key];
+};
+
+// Updating a Post
+exports.update = async (key, title, body) => {
+  posts[key] = new Post(key, title, body);
+  return posts[key];
+};
+
+// Reading Posts
+exports.read = async key => {
+  if (posts[key])
+    return posts[key];
+  else
+    throw new Error(`Post ${key} does not exist`);
+};
+
+//Deleting Posts
+exports.destroy = async key => {
+  if (posts[key])
+    delete posts[key];
+  else
+    throw new Error(`Post ${key} does not exist`);
+};
+
+exports.keylist = async () => { return Object.keys(posts) };
+exports.count = async () => { return posts.length };
+exports.close = async () => {};

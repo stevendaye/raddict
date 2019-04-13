5a7138f321056a5e95b98175324d0f41e0d567c9
Steven <dev.stevendaye@yahoo.com>
2019-04-13 07:09:46 +0100

    Changed the whole stack from CommonJS Modules to ES6 Modules

--- middlewares/utilities.js
+++
@@ -1,9 +0,0 @@
-/* ## Creating the app's utilities ## */
-const config = require("../config");
-
-const templateRoutes = (req, res, next) => {
-  res.locals.routes = config.routes;
-  next();
-};
-
-module.exports.templateRoutes = templateRoutes;

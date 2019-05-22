e008c24d4b8f97f15bb635a43f3fe432dc60d3ce
Steven <dev.stevendaye@yahoo.com>
2019-05-22 15:36:59 +0100

    Included the footer partial file into all other template files

--- posts/controllers/home.mjs
+++ posts/controllers/home.mjs
@@ -1,15 +1,16 @@
 /* ## Setting up the home controller ## */
+import util from "util";
+import DBG from "debug";
 import * as posts from "../models/posts-model-type";
+
+const log = DBG("raddict:homeposts-socket.io");
+const error = DBG("raddict:error-socket.io");
 
 export default {
   async index (req, res, next) {
     try {
-      let keylist = await posts.keylist();
-      let keyPromises = keylist.map( key =>
-        posts.read(key)
-      );
-      let postlist = await Promise.all(keyPromises);
-
+      const postlist = await getKeyTitlesList();
+      
       res.render("index", {
         title: "Raddict | Your Thoughts Sharing Platform ",
         fakecrumb: "Crumb",
@@ -21,3 +22,22 @@ export default {
     }
   }
 };
+
+async function getKeyTitlesList() {
+  const keylist = await posts.keylist();
+  const keyPromises = keylist.map( key =>
+    posts.read(key).then( post => {
+      return { key: post.key, title: post.title };
+    }));
+  return Promise.all(keyPromises);
+}
+
+export function homeSocketio(io) {
+  const emitPostTitles = async () => {
+    const postlist = await getKeyTitlesList();
+    io.of("/home").emit("postitles", { postlist });
+  };
+  posts.events.on("postcreated", emitPostTitles);
+  posts.events.on("postupdated", emitPostTitles);
+  posts.events.on("postdestroyed", emitPostTitles);
+}

bbf2ebfad0cea556dc8a21a130b254a8a9dea0c4
Steven <dev.stevendaye@yahoo.com>
2019-05-23 04:18:08 +0100

    Imported the socket.io function for the post update and deletion

--- posts/socket.io/index.mjs
+++ posts/socket.io/index.mjs
@@ -2,11 +2,12 @@
 import socketio from "socket.io";
 import passportSocketIo from "passport.socketio";
 import cookieParser from "cookie-parser";
-import { homeSocketio } from "../controllers/home";
+import { socketio as homeSocketio } from "../controllers/home";
+import { socketio as postSocketio } from "../controllers/post";
 import { sessionCookieName, sessionStore, sessionSecret } from "../app.mjs"
 
 function startSocket (server) {
-  const io = socketio.listen(server);
+  const io = socketio(server);
   io.use(passportSocketIo.authorize({
     store: sessionStore,
     secret: sessionSecret,
@@ -14,6 +15,7 @@ function startSocket (server) {
     key: sessionCookieName
   }));
   homeSocketio(io);
+  postSocketio(io);
 };
 
 export { startSocket };
